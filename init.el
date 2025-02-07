728d91946e2f77c205787c5c601e53084dfde459
rochelly <rochelly.fernandes@gmail.com>
2025-02-07 12:15:34 -0300

    adicionando primeiro arquivo de configuração

---
+++ init.el
@@ -0,0 +1,30 @@
+(custom-set-faces
+ ;; Comentários em cinza claro
+ '(font-lock-comment-face ((t (:foreground "#9e744b" :italic t))))
+
+ ;; Strings em verde água
+ '(font-lock-string-face ((t (:foreground "#2aa198" :italic t))))
+
+ ;; Palavras-chave em amarelo
+ '(font-lock-keyword-face ((t (:foreground "#b58900" :weight bold))))
+
+ ;; Funções em azul claro
+ '(font-lock-function-name-face ((t (:foreground "#268bd2"))))
+
+ ;; Variáveis em cinza médio
+ '(font-lock-variable-name-face ((t (:foreground "#6468b3"))))
+
+ ;; Constantes em roxo
+ '(font-lock-constant-face ((t (:foreground "#6c71c4"))))
+
+ ;; Tipos de dados em laranja
+ '(font-lock-type-face ((t (:foreground "#cb4b16" :weight bold))))
+
+ ;; Erros e avisos em vermelho
+ '(font-lock-warning-face ((t (:foreground "#dc322f" :weight bold :background "#fdf6e3"))))
+ 
+ )
+
+(set-face-foreground 'default "#b3b3b3")
+
+(set-face-background 'region "#354166")
